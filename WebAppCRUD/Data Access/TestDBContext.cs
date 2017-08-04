c981a7246c899bc8bad7478f62c1fed4df6afe45
Melqui Eugenio <melqui.eug@gmail.com>
2017-08-04 17:17:10 -0300

    Release. Signed-off-by: Melqui Eugenio <melqui.eug@gmail.com>

---
+++ WebAppCRUD/Data Access/TestDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using WebAppCRUD.Models;
+
+namespace WebAppCRUD.Data_Access
+{
+    public class TestDBContext : DbContext
+    {
+        public TestDBContext() : base("TestConnection")
+        {
+
+        }
+
+        public DbSet<Table> Table { get; set; }
+    }
+}
---
+++ WebAppCRUD/Models/Table.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppCRUD.Models
+{
+    public class Table
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string País { get; set; }
+    }
+}
